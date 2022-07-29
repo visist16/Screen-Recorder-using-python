86062bb791618016e39fb7a3f5213a5bc4ac19cd
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-07-29 23:16:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import datetime
 
+from PIL import ImageGrab
+import numpy as np
+import cv2
+from win32api import GetSystemMetrics
+
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+file_name = f'{time_stamp}.mp4'
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (width, height))
+
+webcam = cv2.VideoCapture(1)
